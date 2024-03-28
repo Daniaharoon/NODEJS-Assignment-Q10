fe158876bfd92910592cdb1b75073522118a6f80
Dania Haroon <159708973+Daniaharoon@users.noreply.github.com>
2024-03-28 10:19:21 +0500

    Add files via upload

---
+++ ques10.ts
@@ -0,0 +1,6 @@
+// /  10 .Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+//let myName:string = "Dania";
+//let Date:number = 11-march-2024;
+console.log(`I am learning typescript`);
---
+++ ques10part2.ts
@@ -0,0 +1,5 @@
+
+// let myAge:number = 18;
+//let Date:number = 11-march-2024;
+console.log(2 +5);
+console.log("My name is Dania.");
